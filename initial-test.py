d89251be3651530e190fd3d0e35cda1f9d8eb43c
Raspberrypirate <31635490+Raspberrypirate@users.noreply.github.com>
2017-09-05 00:31:59 +0100

    Create initial-test.py

---
+++ initial-test.py
@@ -0,0 +1,4 @@
+# Set base url as basis on which to make queries
+BASE-URL ="https://maps.googleapis.com/maps/api/distancematrix/json?"
+
+# A little confused about the library here: https://github.com/googlemaps/google-maps-services-python
