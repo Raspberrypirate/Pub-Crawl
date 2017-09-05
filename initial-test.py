b63729712cca63a1b1820bfdc22dd63035dff38d
Raspberrypirate <31635490+Raspberrypirate@users.noreply.github.com>
2017-09-05 01:34:45 +0100

    v0.2
    Made it read origins and destinations from .csv files
    Next steps will be testing and printing results to a csv file.

--- initial-test.py
+++ initial-test.py
@@ -1,15 +1,29 @@
+# Test on the fly with webbrowser
+#url = "https://maps.googleapis.com/maps/api/staticmap?size=600x400"
+#webbrowser.open(url)
+import os
+import csv
+import requests
 from urllib.parse import urlencode
 import webbrowser
 
 # Set base url as basis on which to make queries
 BASE_URL ="https://maps.googleapis.com/maps/api/distancematrix/json?"
 
-# Test on the fly with webbrowser
-#url = "https://maps.googleapis.com/maps/api/staticmap?size=600x400"
-#webbrowser.open(url)
+os.chdir('C:\Users\Nick\Documents\Documents\Decision Support\App Development - Pub Crawl Creator\')
+newfile = csv.DictReader(open('Bristol Pubs - Origins.csv'))
+str_origins = []
+For row in newfile:
+  str_origins.append(row['Origin'])
+  
+newfile = csv.DictReader(open('Bristol Pubs - Destinations.csv'))
+str_destinations = []
+For row in newfile:
+  str_origins.append(row['Destination'])
 
-str_origins = ['Alterego, Bristol', 'The Bank, Bristol']
-str_destinations = ['The Lansdown, Bristol', 'The Crown, Bristol', 'Ye Shakespear, Bristol']
+
+#str_origins = ['Alterego, Bristol', 'The Bank, Bristol']
+#str_destinations = ['The Lansdown, Bristol', 'The Crown, Bristol', 'Ye Shakespear, Bristol']
 
 mydict = ['origins':str_origins,'destinations':str_destinations,'mode':'walking','language':'en-EN','key':'AIzaSyCEkcQlgD77bHxS_g31KwsUWoHvFuiT-YY']
 
