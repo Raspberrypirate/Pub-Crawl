c6250de6ff383ba12f9dd5b30e071b4a370d71bb
Raspberrypirate <31635490+Raspberrypirate@users.noreply.github.com>
2017-09-05 03:10:46 +0100

    v0.3
    in testing. Tested up to "mydict". No idea why that doesn't work.

--- initial-test.py
+++ initial-test.py
@@ -7,19 +7,25 @@ import requests
 from urllib.parse import urlencode
 import webbrowser
 
+print('1')
+
 # Set base url as basis on which to make queries
 BASE_URL ="https://maps.googleapis.com/maps/api/distancematrix/json?"
 
-os.chdir('C:\Users\Nick\Documents\Documents\Decision Support\App Development - Pub Crawl Creator\')
+print('BASE_URL')
+
+os.chdir("C:\\Users\\Nick\\Documents\\Documents\\Decision Support\\App 
+
+Development - Pub Crawl Creator")
 newfile = csv.DictReader(open('Bristol Pubs - Origins.csv'))
 str_origins = []
 For row in newfile:
-  str_origins.append(row['Origin'])
+  str_origins.append(row['Origins'])
   
 newfile = csv.DictReader(open('Bristol Pubs - Destinations.csv'))
 str_destinations = []
 For row in newfile:
-  str_origins.append(row['Destination'])
+  str_destinations.append(row['Destinations'])
 
 
 #str_origins = ['Alterego, Bristol', 'The Bank, Bristol']
@@ -31,4 +37,6 @@ Send_URL = BASE_URL + urlencode(mydict, doseq = True)
 
 webbrowser.open(SEND_URL)
 
-# A little confused about the library here: https://github.com/googlemaps/google-maps-services-python
+# A little confused about the library here: 
+
+https://github.com/googlemaps/google-maps-services-python
